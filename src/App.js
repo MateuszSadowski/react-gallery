fb3c81d9eab327071b81d19a7471ef0584b6bcd2
Mateusz Sadowski <mateusz.sadowski1@gmail.com>
2020-01-22 15:46:29 +0100

    Add display images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [images, setImage] = useState([
+    {
+      url: "https://images.unsplash.com/photo-1579614456650-dfcdfee5db11?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+      alt: "Weird shapes, but cool"
+    },
+    {
+      url: "https://images.unsplash.com/photo-1579681638740-8cbde33e30c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80",
+      alt: "Drone shot!"
+    },
+    {
+      url: "https://images.unsplash.com/photo-1579677083279-e146fc4a2c5f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=363&q=80",
+      alt: "Mountain road, take me home, to the place, na na na na"
+    }
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Gallery">
+        {images.map((image, index) => (
+          <img key={index} src={image.url} alt={image.alt} />
+        ))}
+      </div>
     </div>
   );
 }
