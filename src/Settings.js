dc9421a1313932e96a4c01b35717caae74f096ad
Mateusz Sadowski <mateusz.sadowski1@gmail.com>
2020-02-04 11:35:34 +0100

    Fix use smaller pic sizes for performance

--- src/Settings.js
+++ src/Settings.js
@@ -1,4 +1,4 @@
 export const VIDEOPLAYER_WIDTH = 640;
 export const VIDEOPLAYER_VOLUME = 0.6;
-export const VIMEO_THUMBNAIL_SIZE = 4;
+export const VIMEO_THUMBNAIL_SIZE = 3;
 export const PARALAX_FACTOR = 0.025;
